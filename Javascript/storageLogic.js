ff484b5d4bc49b6f9a9ea09f0ff76cf4ba2458ec
Ani07-05 <86768646+Ani07-05@users.noreply.github.com>
2023-12-13 22:21:09 +0530

    Delete Javascript directory

--- Javascript/storageLogic.js
+++
@@ -1,7 +0,0 @@
-function saveToLocalStorage(key, data) {
-    localStorage.setItem(key, JSON.stringify(data));
-}
-
-function getFromLocalStorage(key) {
-    return JSON.parse(localStorage.getItem(key)) || [];
-}
